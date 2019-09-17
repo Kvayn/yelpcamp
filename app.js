afe82145e58405d05e21e13d48b2bc0fd2ef668d
Serhii <sdorodko@gmail.com>
2019-09-17 14:56:06 +0300

    Authentication added

--- app.js
+++ app.js
@@ -2,21 +2,39 @@ var express = require("express")
 var app = express()
 var bodyParser = require("body-parser")
 var mongoose = require("mongoose")
+var passport = require("passport")
+var LocalStrategy = require("passport-local")
 
 var Campground = require("./models/campground")
 var Comment = require("./models/comment")
+var User = require("./models/user")
 var seedDB = require("./seeds")
 
 seedDB();
 
+//Passport configuration
+app.use(require("express-session")({
+	secret: "There is no spoon",
+	resave: false,
+	saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user
+	next()
+})
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true })
 
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
-
-app.listen(8080, function(){
+app.use(express.static(__dirname + "/public"))
+app.listen(8000, function(){
 	console.log("Server has started succsessfuly")
 
 })
@@ -69,7 +87,7 @@ app.get("/campgrounds/:id", function(req, res){
 //=========================
 //Comments ROUTS
 //=========================
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLogedIn, function(req, res){
 	Campground.findById(req.params.id, function(err, campground){
 		if (err) {
 			console.log(err)
@@ -79,7 +97,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
 	})	
 })
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLogedIn, function(req, res){
 	//lookup campgriund using id
 	Campground.findById(req.params.id, function(err, campground){
 		if (err) {
@@ -101,3 +119,44 @@ app.post("/campgrounds/:id/comments", function(req, res){
 	//connect new comment to compground
 	//redirect
 })
+
+//AUTH ROUTES
+app.get("/register", function(req, res){
+	res.render("register");
+}) 
+
+app.post("/register", function(req, res){
+	var newUser = new User({username: req.body.username})
+	User.register(newUser, req.body.password, function(err, user){
+		if (err) {
+			console.log(err)
+			return res.render("register")
+		}
+		passport.authenticate("local")(req, res, function(){
+			res.redirect("/campgrounds")
+		})
+	})
+})
+//Login form
+app.get("/login", function(req, res){
+	res.render("login")
+})
+app.post("/login", passport.authenticate("local", {
+	successRedirect: "/campgrounds",
+	failureRedirect: "/login"
+}), function(req, res){
+
+})
+
+//Logout rout
+app.get("/logout", function(req, res){
+	req.logout()
+	res.redirect("/campgrounds")
+})
+
+function isLogedIn(req, res, next){
+	if (req.isAuthenticated()) {
+		return next()
+	}
+	res.redirect("/login")
+}
